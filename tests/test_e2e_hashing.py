1ffb7ae043d577e57b905e1d58f80008fbe178c9
Brian Thorne <brian@thorne.link>
2017-11-16 18:40:13 +1100

    Update test name

--- tests/test_e2e_hashing.py
+++ tests/test_e2e_hashing.py
@@ -5,10 +5,9 @@ import math
 from clkhash import clk, randomnames, bloomfilter, schema
 
 
-class TestChunks(unittest.TestCase):
+class TestNamelistHashable(unittest.TestCase):
 
-    def test_e2e(self):
+    def test_namelist_hashable(self):
         namelist = randomnames.NameList(1000)
         s1, s2 = namelist.generate_subsets(100, 0.8)
         bf1 = bloomfilter.calculate_bloom_filters(s1, namelist.schema_types, ('secret', 'sshh'))
-
