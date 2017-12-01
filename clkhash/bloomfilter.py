a03a5fea7c581c5611355b5b33d7d3d3d7b045f5
wilko <wilko.henecka@data61.csiro.au>
2017-12-01 15:06:56 +1100

    we pass lists of keys to hbloom now. One for each identifier

--- clkhash/bloomfilter.py
+++ clkhash/bloomfilter.py
@@ -5,35 +5,35 @@ Generate a Bloom filter
 """
 import base64
 import hmac
+import sys
 from hashlib import sha1, md5
 
 from bitarray import bitarray
 
-import sys
 
-def hbloom(mlist, l=1024, k=30, keysha1="secret1", keymd5="secret2"):
+def hbloom(mlist, keys_sha1, keys_md5, l=1024, k=30):
     """
     Cryptographic bloom filter for list of strings
 
     :param mlist: list of strings to be hashed and encoded in filter
+    :param keys_sha1: list of hmac secret keys for sha1, one for each element in mList
+    :param keys_md5: list of hmac secret keys for md5, one for each element in mList
     :param l: length of filter
     :param k: number of hash functions to use per element
-    :param keysha1: hmac secret key for sha1
-    :param keymd5: hmac secret key for md5
     :return: bitarray with bloom filter
     """
     bf = bitarray(l)
     bf[:] = 0
-    for m in mlist:
-        sha1hm = int(hmac.new(keysha1.encode(), m.encode(), sha1).hexdigest(), 16) % l
-        md5hm = int(hmac.new(keymd5.encode(), m.encode(), md5).hexdigest(), 16) % l
+    for m, key_sha, key_md5 in zip(mlist, keys_sha1, keys_md5):
+        sha1hm = int(hmac.new(key_sha, m.encode(), sha1).hexdigest(), 16) % l
+        md5hm = int(hmac.new(key_md5, m.encode(), md5).hexdigest(), 16) % l
         for i in range(k):
             gi = (sha1hm + i * md5hm) % l
             bf[gi] = 1
     return bf
 
 
-def cryptoBloomFilter(record, tokenizers, key1="test1", key2="test2"):
+def crypto_bloom_filter(record, tokenizers, keys1, keys2):
     """
     Make a bloom filter from a record with given tokenizers
 
@@ -42,8 +42,8 @@ def cryptoBloomFilter(record, tokenizers, key1="test1", key2="test2"):
 
     :param record: plaintext record tuple. E.g. (index, name, dob, gender)
     :param tokenizers: A list of IdentifierType tokenizers (one for each record element)
-    :param key1: key for first hash function
-    :param key2: key for second hash function
+    :param keys1: list of keys for first hash function
+    :param keys2: list of keys for second hash function
 
     :return: 3-tuple - bitarray with bloom filter for record, index of record, bitcount
     """
@@ -53,7 +53,7 @@ def cryptoBloomFilter(record, tokenizers, key1="test1", key2="test2"):
         for token in tokenizer(entry):
             mlist.append(token)
 
-    bf = hbloom(mlist, keysha1=key1, keymd5=key2)
+    bf = hbloom(mlist, keys_sha1=keys1, keys_md5=keys2)
 
     return bf, record[0], bf.count()
 
@@ -64,18 +64,18 @@ def stream_bloom_filters(dataset, schema_types, keys):
 
     :param dataset: An iterable of indexable records.
     :param schema: An iterable of identifier type names.
-    :param keys: A tuple of two secret keys used in the HMAC.
+    :param keys: A tuple of two lists of secret keys used in the HMAC.
     :return: Yields bloom filters as 3-tuples
     """
     for s in dataset:
-        yield cryptoBloomFilter(s, schema_types, key1=keys[0], key2=keys[1])
+        yield crypto_bloom_filter(s, schema_types, keys1=keys[0], keys2=keys[1])
 
 
 def calculate_bloom_filters(dataset, schema, keys):
     """
     :param dataset: A list of indexable records.
     :param schema: An iterable of identifier types.
-    :param keys: A tuple of two secret keys used in the HMAC.
+    :param keys: A tuple of two lists of secret keys used in the HMAC.
     :return: List of bloom filters as 3-tuples, each containing
              bloom filter (bitarray), index (int), bitcount (int)
     """
