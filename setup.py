f337722963cb1e7c15483cf9a3b480fbcd66eb56
Brian Thorne <brian@thorne.link>
2017-12-11 20:38:12 +1100

    Release v0.8.0 of clkhash (#25)
    * Add apache license files
    * Remove old ci test script and add setup.cfg
    * Update readme with api example
    * Add changelog for v0.8.0
    * Update requirements

--- setup.py
+++ setup.py
@@ -2,21 +2,20 @@ from setuptools import setup, find_packages
 
 requirements = [
         "bitarray==0.8.1",
-        "networkx==1.11",
         "click==6.7",
         "requests==2.18.1",
-        "futures==3.1.1"
+        "futures==3.1.1",
+        "cryptography==2.1.3"
     ]
 
 setup(
     name="clkhash",
-    version='0.7.3',
+    version='0.8.0-rc.1',
     description='Hash utility to create Cryptographic Linkage Keys',
     url='https://github.com/n1analytics/clkhash',
     license='Apache',
-    setup_requires=requirements,
     install_requires=requirements,
-    test_requires=requirements,
+    test_requires=['nose>=1.3'],
     packages=find_packages(exclude=['tests']),
     package_data={'clkhash': ['data/*.csv']},
     entry_points={
