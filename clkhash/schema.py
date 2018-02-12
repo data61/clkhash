9c13aeb215d272f85bf4d15c3a9ae1d1eb1fc884
Jakub Nabaglo <jakub.nabaglo@data61.csiro.au>
2018-02-12 15:46:55 +1100

    Documentation for schema.py

--- clkhash/schema.py
+++ clkhash/schema.py
@@ -1,29 +1,61 @@
+# -*- coding: utf-8 -*-
+
+""" Schema loading and validation.
+"""
+
 import json
+from typing import Any, Dict, Hashable, List, TextIO
 
 from future.utils import raise_from
 import jsonschema
 
+from clkhash import field_formats
+
 # These are relative to this file.
 MASTER_SCHEMA_PATHS = {1: 'master-schema-v1.json'}
 
+MASTER_SCHEMA_DIRECTORY = os.path.dirname(__file__)
+
 
 def get_master_schema_path(version):
-    if version in MASTER_SCHEMA_PATHS:
-        directory = os.path.dirname(__file__)
-        return os.path.join(directory, MASTER_SCHEMA_PATHS[version])
-    else:
+    # type: (Hashable) -> str
+    """ Get the path of the master schema given a version.
+
+        :param version: The version of the master schema whose path we
+            wish to retrieve.
+    """
+    try:
+        rel_path = MASTER_SCHEMA_PATHS[version]
+    except (TypeError, KeyError):
         return None
+    else:
+        return os.path.join(MASTER_SCHEMA_DIRECTORY, rel_path)
 
 
 class SchemaError(Exception):
+    """ The user-defined schema is invalid.
+    """
     pass
 
 
 class MasterSchemaError(Exception):
+    """ Master schema missing? Corrupted? Otherwise surprising? This is
+        the exception for you!
+    """
     pass
 
 
 def validate_schema(schema):
+    # type: (Dict[str, Any]) -> None
+    """ Validate the schema.
+
+        This raises iff either the schema or the master schema are
+        invalid. If it's successful, it returns nothing.
+
+        :param schema: The schema to validate, as parsed by `json`.
+        :raises SchemaError: When the schema is invalid.
+        :raises MasterSchemaError: When the master schema is invalid.
+    """
     if type(schema) is not dict:
         msg = ('The top level of the schema file is a {}, whereas a dict is '
                'expected.'.format(type(schema).__name__))
@@ -57,12 +89,20 @@ def validate_schema(schema):
     except jsonschema.exceptions.ValidationError as e:
         raise_from(SchemaError('The schema is not valid.'), e)
     except jsonschema.exceptions.SchemaError as e:
-        raise_from(MasterSchemaError('The master schema is not valid. The '
-                                     'schema cannot be validated. Please file '
-                                     'a bug report.'), e)
+        msg = ('The master schema is not valid. The schema cannot be '
+               'validated. Please file a bug report.')
+        raise_from(MasterSchemaError(msg), e)
 
 
 def load_schema(schema_file):
+    # type: (TextIO) -> List[field_formats.FieldSpec]
+    """ Loads and validates a schema.
+
+        The schema file is loaded as a list of `FieldSpec` objects.
+
+        :param schema_file: The schema file to load.
+        :returns: A list of `FieldSpec`s, one for each field.
+    """
     try:
         schema = json.load(schema_file)
     except json.decoder.JSONDecodeError as e:
