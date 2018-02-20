8eb490723d5f924b95f4c74b47e929b2c30f3b90
wilko <wilko.henecka@data61.csiro.au>
2018-02-20 14:51:47 +1100

    added some path discovery magic

--- tests/test_json_schema.py
+++ tests/test_json_schema.py
@@ -9,8 +9,9 @@ from jsonschema.exceptions import ValidationError, SchemaError
 class TestJsonSchema(unittest.TestCase):
 
     def test_example_validation(self):
-        with open(os.path.join(os.path.abspath('..'), 'docs/_static/schema.json')) as schema_file, \
-                open(os.path.join(os.path.abspath('..'), 'docs/_static/example_schema.json')) as example_file:
+        clkhash_path = os.path.abspath(os.path.dirname(__file__) + '/..')
+        with open(os.path.join(clkhash_path, 'docs/_static/schema.json')) as schema_file, \
+                open(os.path.join(clkhash_path, 'docs/_static/example_schema.json')) as example_file:
             schema = json.load(schema_file)
             example = json.load(example_file)
             try:
