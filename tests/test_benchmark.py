482980771fc2614c61aed6f1b1977dd0caa57238
wilko <wilko.henecka@data61.csiro.au>
2018-02-20 18:19:47 +1100

    benchmark test will not run on Travis

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3.4
 
 import unittest
+import os
 
 from clkhash import benchmark
 
 
 class TestBenchmark(unittest.TestCase):
 
+    @unittest.skipIf("TRAVIS" in os.environ and os.environ['TRAVIS'] == 'true',
+                     "Travis has no muscles to speak of...")
     def test_benchmarking_hash(self):
         speed = benchmark.compute_hash_speed(1000, quiet=True)
         self.assertGreater(speed, 100, "Hashing at less than 100 H/s")
