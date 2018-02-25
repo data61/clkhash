32b5d9a57e6b42b9bc6e121ddc5e849b537d13ce
Brian Thorne <brian@thorne.link>
2018-02-26 10:34:48 +1100

    Run benchmark but allow it to fail on CI systems

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3.4
 
 import unittest
+import pytest
 import os
 
 from clkhash import benchmark
 
+IS_APPVEYOR = 'APPVEYOR' in os.environ
+IS_TRAVIS = 'TRAVIS' in os.environ
+ON_CI = IS_APPVEYOR or IS_TRAVIS
+
 
 class TestBenchmark(unittest.TestCase):
 
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ['TRAVIS'] == 'true',
-                     "Travis has no muscles to speak of...")
     def test_benchmarking_hash(self):
         speed = benchmark.compute_hash_speed(1000, quiet=True)
-        self.assertGreater(speed, 100, "Hashing at less than 100 H/s")
-
+        try:
+            assert speed > 100, "Hashing at less than 100 H/s"
+        except AssertionError:
+            if ON_CI:
+                pytest.mark.xfail(ON_CI, reason="CI have inconsistent and weak muscles...")
+            else:
+                raise
