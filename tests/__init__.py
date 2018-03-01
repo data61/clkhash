c4eec2164b8464ed78ae853f3919ec3b6590acc1
Brian Thorne <brian@thorne.link>
2018-03-01 13:15:59 +1100

    Cross platform temporary file

---
+++ tests/__init__.py
@@ -0,0 +1,38 @@
+import os
+import tempfile
+
+
+class temporary_file(object):
+    """
+        A cross platform temporary file context manager.
+
+        Usage:
+
+            with temporary_file() as filename:
+                # use the file
+
+            # file is now deleted
+
+        We don't use NamedTemporaryFile as it can't be opened multiple times on windows
+        #return tempfile.NamedTemporaryFile()
+
+        :return: (file name)
+        """
+
+    def __enter__(self):
+        print('Starting')
+        self.os_fd, self.tmpfile_name = tempfile.mkstemp(text=True)
+
+        return self.tmpfile_name
+
+    def __exit__(self, *exc):
+        print('Finishing')
+        os.close(self.os_fd)
+        os.remove(self.tmpfile_name)
+        return False
+
+
+def create_temp_file():
+    os_fd, filename = tempfile.mkstemp(text=True)
+
+    return open(filename, 'w')
