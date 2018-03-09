02966ebcd67e11d87ff958449729747a10ec43ba
Brian Thorne <brian@thorne.link>
2018-03-09 14:28:26 +1100

    Update version for 0.10.0 release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements = [
 
 setup(
     name="clkhash",
-    version='0.10.0-rc.1',
+    version='0.10.0',
     description='Hash utility to create Cryptographic Linkage Keys',
     url='https://github.com/n1analytics/clkhash',
     license='Apache',
