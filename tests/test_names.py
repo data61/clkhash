425ca982a29f5c150c0238bb9f4c053339eb1b0f
Jakub Nabaglo <jakub.nabaglo@data61.csiro.au>
2018-05-03 12:50:27 +1000

    Multiparty testing with randomnames.

--- tests/test_names.py
+++ tests/test_names.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import itertools
 import math
 import unittest
 
@@ -52,6 +53,28 @@ class TestRandomNames(unittest.TestCase):
                     counteq += 1
 
         self.assertEqual(counteq, 500)
+
+
+class TestRandomNamesMultiparty(unittest.TestCase):
+    def test_generate_subsets(self):
+        nl = rn.NameList(50)
+        subsets = map(set, nl.generate_subsets(10, 0.8, subsets=5))
+
+        for s1, s2 in itertools.combinations(subsets, 2):
+            self.assertEqual(len(s1 & s2), 8, msg='unexpected overlap size')
+
+    def test_generate_subsets_raises(self):
+        names = rn.NameList(15)
+        with pytest.raises(ValueError):
+            names.generate_subsets(10, 0.8, subsets=5)
+
+    def test_generate_large_subsets(self):
+        nl = rn.NameList(5000)
+        subsets = map(set, nl.generate_subsets(1000, 0.5, subsets=3))
+
+        for s1, s2 in itertools.combinations(subsets, 2):
+            self.assertEqual(len(s1 & s2), 500, msg='unexpected overlap size')
+
 
 if __name__ == '__main__':
     unittest.main()
