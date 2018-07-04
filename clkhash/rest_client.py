8c6b39fd253aadcc5023d44e1b78222c51633470
Brian Thorne <brian@thorne.link>
2018-07-04 10:41:33 +1000

    Better error message for rest_client ServiceErrors Add project & run deletion calls, and project get description.

--- clkhash/rest_client.py
+++ clkhash/rest_client.py
@@ -10,16 +10,26 @@ class ServiceError(Exception):
     def __init__(self, msg, response):
         self.status_code = response.status_code
         self.text = response.text
-
         super(ServiceError, self).__init__(msg)
+
+    def __str__(self):
+        return "{}\nStatus Code: {}\nServer Message:\n{}".format(self.args[0], self.status_code, self.text)
+
+
+def _handle_json_response(response, failure_message, expected_status_code=200):
+    if response.status_code == 503:
+        raise ServiceError('Client is rate limited. Try requesting less frequently.', response)
+    if response.status_code != expected_status_code:
+        raise ServiceError(failure_message, response)
+    try:
+        return response.json()
+    except ValueError:
+        raise ServiceError(failure_message, response)
 
 
 def server_get_status(server):
     response = requests.get(server + "/api/v1/status")
-    if response.status_code != 200:
-        raise ServiceError("Service Status Error", response)
-
-    return response.json()
+    return _handle_json_response(response, "Error with service status")
 
 
 def project_create(server, schema, result_type, name, notes=None, parties=2):
@@ -36,10 +46,28 @@ def project_create(server, schema, result_type, name, notes=None, parties=2):
             'notes': notes
         }
     )
-    if response.status_code != 201:
-        raise ServiceError("Error creating project", response)
-    # Parse project created response
-    return response.json()
+    return _handle_json_response(response, "Error creating project", 201)
+
+
+def project_delete(server, project, apikey):
+    response = requests.delete(
+        '{}/api/v1/projects/{}'.format(server, project),
+        headers={"Authorization": apikey}
+    )
+    if response.status_code != 200:
+        raise ServiceError("Error deleting project", response)
+    return response.text
+
+
+def project_get_description(server, project, apikey):
+    response = requests.get(
+        '{}/api/v1/projects/{}'.format(server, project),
+        headers={
+            "Authorization": apikey,
+            'content-type': 'application/json'
+        }
+    )
+    return _handle_json_response(response, "Error getting project description", 200)
 
 
 def project_upload_clks(server, project, apikey, clk_data):
@@ -51,9 +79,7 @@ def project_upload_clks(server, project, apikey, clk_data):
             'content-type': 'application/json'
         }
     )
-    if response.status_code != 201:
-        raise ServiceError("Error uploading CLKS to project", response)
-    return response.json()
+    return _handle_json_response(response, "Error uploading CLKS to project", 201)
 
 
 def run_create(server, project_id, apikey, threshold, name, notes=None):
@@ -69,11 +95,7 @@ def run_create(server, project_id, apikey, threshold, name, notes=None):
             'notes': notes
         }
     )
-
-    if response.status_code != 201:
-        raise ServiceError("Unexpected response while creating run", response)
-
-    return response.json()
+    return _handle_json_response(response, "Unexpected response while creating run", 201)
 
 
 def run_get_status(server, project, run, apikey):
@@ -81,9 +103,7 @@ def run_get_status(server, project, run, apikey):
         '{}/api/v1/projects/{}/runs/{}/status'.format(server, project, run),
         headers={"Authorization": apikey}
     )
-    if response.status_code != 200:
-        raise ServiceError("Run Status Error", response)
-    return response.json()
+    return _handle_json_response(response, "Run Status Error", 200)
 
 
 def run_get_result_text(server, project, run, apikey):
@@ -91,9 +111,18 @@ def run_get_result_text(server, project, run, apikey):
         '{}/api/v1/projects/{}/runs/{}/result'.format(server, project, run),
         headers={"Authorization": apikey}
     )
-
     if response.status_code != 200:
         raise ServiceError("Error retrieving results", response)
+    return response.text
+
+
+def run_delete(server, project, run, apikey):
+    response = requests.delete(
+        '{}/api/v1/projects/{}/runs/{}'.format(server, project, run),
+        headers={"Authorization": apikey}
+    )
+    if response.status_code != 200:
+        raise ServiceError("Error deleting run", response)
     return response.text
 
 
@@ -102,7 +131,7 @@ def format_run_status(status):
         "State: {}".format(status['state']),
         "Stage {} ({}/{})".format(
             status['current_stage']['description'],
-            status['current_stage'],
+            status['current_stage']['number'],
             status['stages']
         )
     ]
