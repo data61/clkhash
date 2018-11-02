af9b151b3efc938f73a4c49d018ec86c9d3b4da9
Brian Thorne <brian@thorne.link>
2018-11-02 20:37:09 +1100

    Give appveyor a test deploy

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with codecs.open('README.md', 'r', 'utf-8') as f:
 
 setup(
     name="clkhash",
-    version='0.12.1',
+    version='0.12.2-dev',
     description='Hash utility to create Cryptographic Linkage Keys',
     long_description=readme,
     long_description_content_type='text/markdown',
