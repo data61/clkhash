e3354621ba882ca89a37f8b845d692065d02d5b5
Jake <git@programmedbyjake.com>
2019-07-05 22:50:36 +1000

    Add test cases for distributions

--- tests/test_names.py
+++ tests/test_names.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from io import BytesIO
 import itertools
 import math
 import unittest
@@ -10,6 +12,45 @@ __author__ = 'shardy'
 
 
 class TestRandomNames(unittest.TestCase):
+    def test_save_csv(self):
+        bytesIO = BytesIO()
+        headers = ['Name', 'Count']
+        body = [('a', 1), ('b', 2)]
+        rn.save_csv(body, headers, bytesIO)
+        self.assertListEqual(bytesIO.getvalue().split(), ['Name,Count', 'a,1', 'b,2'])
+
+    def test_random_date(self):
+        # String as age value
+        age_dist = rn.Distribution('../tests/testdata/ages_dirty.csv')
+        with pytest.raises(ValueError):
+            rn.random_date(2018, age_dist)
+
+        # Ensure dates fall in range
+        age_dist = rn.Distribution('../tests/testdata/ages.csv')
+        start = datetime(2016, 1, 1)
+        end = datetime(2019, 1, 1)
+
+        for i in range(1000):
+            self.assertGreaterEqual((rn.random_date(2018, age_dist)-start).days, 0)
+            self.assertLess((rn.random_date(2018, age_dist)-end).days, 0)
+
+    def test_distribution(self):
+        # No distribution
+        age_dist = None
+        with pytest.raises(ValueError):
+            rn.random_date(2018, age_dist)
+
+        # File with no content
+        with pytest.raises(ValueError):
+            rn.Distribution('../tests/testdata/dist_empty.csv')
+
+        # File with only headers
+        with pytest.raises(ValueError):
+            rn.Distribution('../tests/testdata/dist_empty_headers.csv')
+
+        # File with non-integer count
+        with pytest.raises(ValueError):
+            rn.Distribution('../tests/testdata/dist_dirty.csv')
 
     def test_generate_subsets(self):
         nl = rn.NameList(20)
