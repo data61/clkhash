745e7800b4f07a5c5c392afd46a70d64d580ff0e
wilko <wilko.henecka@data61.csiro.au>
2019-10-30 11:06:47 +1100

    fixed test

--- tests/test_field_formats.py
+++ tests/test_field_formats.py
@@ -2,6 +2,7 @@
 
 import math
 import unittest
+from decimal import Decimal
 
 from clkhash import field_formats, comparators
 
@@ -251,7 +252,7 @@ class TestFieldFormats(unittest.TestCase):
                 'description': 'buzz'
             },
             'hashing': {
-                'comparison': {'type': 'numeric', 'thresholdDistance': 100.4, 'resolution': 25},
+                'comparison': {'type': 'numeric', 'thresholdDistance': '100.4', 'resolution': 25},
                 'strategy': {'bitsPerToken': 20}
             }
         }
@@ -316,7 +317,7 @@ class TestFieldFormats(unittest.TestCase):
         # Check the hashing specs.
         comparator = spec.hashing_properties.comparator
         self.assertIsInstance(comparator, comparators.NumericComparison)
-        self.assertEqual(comparator.threshold_distance, 100.4)
+        self.assertEqual(comparator.threshold_distance, Decimal('100.4'))
         self.assertEqual(comparator.resolution, 25)
         self.assertIsInstance(spec.hashing_properties.strategy, field_formats.BitsPerTokenStrategy)
         self.assertEqual(spec.hashing_properties.strategy._bits_per_token, 20)
