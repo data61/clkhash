51fd41c0054893d3847ef2282aa4d58ffe02b5db
Brian Thorne <brian@hardbyte.nz>
2020-03-30 14:27:34 +1300

    Fix typing issue for more versions of python and mypy

--- clkhash/backports.py
+++ clkhash/backports.py
@@ -3,7 +3,7 @@ import re
 import sys
 import time
 from datetime import datetime
-from typing import AnyStr, Callable, cast, Pattern, Sequence, Text, Union, SupportsBytes, Iterable
+from typing import AnyStr, Callable, cast, Pattern, Sequence, Text, Union, Iterable
 
 from future.utils import raise_from as _raise_from
 from mypy_extensions import Arg, DefaultNamedArg, NoReturn
@@ -39,11 +39,15 @@ except AttributeError:
         hex_str = codecs.encode(bytes, 'hex')  # type: ignore
         return int(hex_str, 16)
 
-
+    try:
+        from typing import SupportsBytes
+        bytes_arg_type = Union[Iterable[int], SupportsBytes]
+    except ImportError:
+        bytes_arg_type = Iterable[int]
     # Make this cast since Python 2 doesn't have syntax for default
     # named arguments. Hence, must cast so Mypy thinks it matches the
     # original function.
-    int_from_bytes = cast(Callable[[Arg(Union[Iterable[int], SupportsBytes], 'bytes'),
+    int_from_bytes = cast(Callable[[Arg(bytes_arg_type, 'bytes'),
                                     Arg(str, 'byteorder'),
                                     DefaultNamedArg(bool, 'signed')],
                                    int],
