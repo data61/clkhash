65e1ccff7e7287f3be5db3d745b8f5bdc5a3f17d
wilko <wilko.henecka@data61.csiro.au>
2020-06-16 15:00:20 +1000

    remove outdated requirements

--- setup.py
+++ setup.py
@@ -1,32 +1,21 @@
 from setuptools import setup, find_packages
-import sys
-if sys.version_info[0] < 3:
-  import codecs
-  
 
 requirements = [
         "bashplotlib>=0.6.5",
         "bitarray-hardbyte>=1.0.0",             # Fork of bitarray distributing binary wheels #153
         "cryptography>=2.3",
-        "enum34==1.1.6; python_version < '3.4'",
         "future>=0.16",
-        "futures>=3.1; python_version < '3.2'",  # Backport from Py3.2
         "jsonschema>=3.0.2",
         "mypy_extensions>=0.3",
         "pyblake2>=1.1.1; python_version < '3.6'",
         "tqdm>=4.24",
-        "typing>=3.6; python_version < '3.5'"  # Backport from Py3.5
     ]
-if sys.version_info[0] < 3:
-    # Python 2.7 does not accept the parameter `encoding` to open a file
-    with codecs.open('README.md', 'r', 'utf-8') as f:
-        readme = f.read()
-else:
-    # But on Windows, something wrong happens when creating the package
-    # and using codecs.open. related issues are https://github.com/di/markdown-description-example/issues/4
-    # But this is not exactly the same either...
-    with open('README.md', 'r', encoding='utf-8') as f:
-        readme = f.read()
+
+# But on Windows, something wrong happens when creating the package
+# and using codecs.open. related issues are https://github.com/di/markdown-description-example/issues/4
+# But this is not exactly the same either...
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
 
 setup(
     name="clkhash",
@@ -48,8 +37,6 @@ setup(
     },
     classifiers=[
         "Development Status :: 5 - Production/Stable",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
