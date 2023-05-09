8a58e78ddd6b4a715270facc263d4de09ffad15a
wilko77 <3759021+wilko77@users.noreply.github.com>
2023-05-09 16:45:54 +1000

    catch errors at teardown

--- tests/test_clk.py
+++ tests/test_clk.py
@@ -107,7 +107,10 @@ class TestComplexSchemaChanges(unittest.TestCase):
         self.SECRET = 'chicken'
 
     def tearDown(self):
-        os.remove(self.CSV_FILE)
+        try:
+            os.remove(self.CSV_FILE)
+        finally:
+            os.remove(self.CSV_FILE)
 
     def test_expected_number_of_encodings_returned(self):
         loaded_schema = schema.from_json_dict(self.SCHEMA_DICT)
@@ -198,10 +201,16 @@ class TestHeaderChecking(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls) -> None:
-        os.remove(cls.csv_correct_header)
-        os.remove(cls.csv_incorrect_header_name)
-        os.remove(cls.csv_incorrect_count)
-        os.remove(cls.csv_no_header)
+        try:
+            os.remove(cls.csv_correct_header)
+            os.remove(cls.csv_incorrect_header_name)
+            os.remove(cls.csv_incorrect_count)
+            os.remove(cls.csv_no_header)
+        finally:
+            os.remove(cls.csv_correct_header)
+            os.remove(cls.csv_incorrect_header_name)
+            os.remove(cls.csv_incorrect_count)
+            os.remove(cls.csv_no_header)
 
     def test_header(self):
         out = clk.generate_clk_from_csv(
